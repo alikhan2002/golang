0e2d2dc8af4a544398ed1b38d6f622f344ae68dd
Alikhan <alihan.zhamankhan@gmail.com>
2023-10-15 22:34:24 +0600

    ch 5.2

--- Assignment#3/cmd/api/main.go
+++ Assignment#3/cmd/api/main.go
@@ -18,7 +18,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
 	}
 }
 type application struct {
@@ -27,12 +30,17 @@ type application struct {
 }
 
 func main() {
-	//GREENLIGHT_DB_DSN = 'postgres://stroller:pa55word@localhost/stroller'
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	//flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://stroller:pa55word@localhost/stroller?sslmode=disable", "PostgreSQL DSN")
+
+	//GREENLIGHT_DB_DSN = 'postgres://stroller:pa55word@localhost/stroller?sslmode=disable'
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	//flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://stroller:pa55word@localhost/stroller?sslmode=disable", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Parse()
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	db, err := openDB(cfg)
@@ -56,6 +64,7 @@ func main() {
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
+
 func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
