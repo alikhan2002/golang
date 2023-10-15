9a7f8c7661aff48035c8ff1a12b6c9a13faa526f
Alikhan <alihan.zhamankhan@gmail.com>
2023-10-15 22:15:55 +0600

    ch 5.2

---
+++ cmd/api/strollers.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"assignment2.alikhan.net/internal/data"
+	"assignment2.alikhan.net/internal/validator"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) createStrollerHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title string `json:"title"`
+		Price int    `json:"price"`
+		Brand string `json:"brand"`
+		Color string `json:"color"`
+		Ages  string `json:"ages"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	stroller := &data.Stroller{
+		Title: input.Title,
+		Brand: input.Brand,
+		Color: input.Color,
+		Ages:  input.Ages,
+		Price: int32(input.Price),
+	}
+	v := validator.New()
+	if data.ValidateStroller(v, stroller); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showStrollerHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	stroller := data.Stroller{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Price:     1000,
+		Title:     "CoolShare Baby Stroller for Toddler",
+		Brand:     "CoolShare",
+		Color:     "Gray",
+		Ages:      "1-3",
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"stroller": stroller}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
